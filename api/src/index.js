5c0a2de2d3a503a51b2e6d37c4d3e6fdc60e6f86
jong-k <kjhan710@naver.com>
2022-11-07 22:13:05 +0900

    :construction: 계정 인증 진행중

--- api/src/index.js
+++ api/src/index.js
@@ -1,57 +1,43 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const db = require('./db');
+const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
+db.connect(DB_HOST);
 
-// 더미 데이터
-let notes = [
-  { id: '1', content: 'This is a note', author: 'Elon Musk' },
-  { id: '2', content: 'This is another note', author: 'Jack Dorsey' },
-  { id: '3', content: 'Oh hey look, another note!', author: 'Tim Cook' },
-];
-
-// 스키마 구성
-const typeDefs = gql`
-  type Note {
-    id: ID!
-    content: String!
-    author: String!
+// JWT에서 사용자 정보 가져오기
+const getUser = (token) => {
+  if (token) {
+    try {
+      // 토큰에서 얻은 사용자 정보 반환
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      throw new Error('Session invalid');
+    }
   }
-
-  type Query {
-    hello: String
-    notes: [Note!]!
-    note(id: ID!): Note!
-  }
-
-  type Mutation {
-    newNote(content: String!): Note!
-  }
-`;
-
-// 스키마 필드를 위한 리졸버 함수 제공
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-    notes: () => notes,
-    note: (parent, args) => {
-      return notes.find((note) => note.id === args.id);
-    },
-  },
-  Mutation: {
-    newNote: (parent, args) => {
-      let noteValue = {
-        id: String(notes.length + 1),
-        content: args.content,
-        author: args.content,
-      };
-      notes.push(noteValue);
-      return noteValue;
-    },
-  },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    // 헤더에서 사용자 토큰 가져오기
+    const token = req.headers.authorization;
+    // 토큰에서 사용자 얻기
+    const user = getUser(token);
+    console.log(user);
+    // context에 db models 및 user 추가
+    return { models, user };
+  },
+});
+// 여기
 server.start().then((res) => {
   const app = express();
   // 아폴로 그래프QL 미들웨어를 적용하고 경로를 /api로 설정
