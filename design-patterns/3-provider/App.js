01efbaad7a4dd846c5a0a5b69dfec1b53644afc7
jhkim-hdxwill <jhkim00@iwillmed.com>
2023-09-15 18:08:45 +0900

    temp commit

---
+++ design-patterns/3-provider/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import List from "./List";
+import Toggle from "./Toggle";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Toggle />
+      <List />
+    </div>
+  );
+}
---
+++ design-patterns/3-provider/Toggle.js
@@ -0,0 +1,8 @@
+export default function Toggle() {
+  return (
+    <label className="switch">
+      <input type="checkbox" />
+      <span className="slider round"></span>
+    </label>
+  );
+}
