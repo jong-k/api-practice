16bf0e88c8477448ca00eba1557a5e2ed88347b9
jhkim-hdxwill <jhkim00@iwillmed.com>
2023-10-04 12:13:06 +0900

    chore: 프로젝트 변경

--- useThemeContext.js
+++
@@ -1,8 +0,0 @@
-import {useContext} from "react";
-import {ThemeContext} from "./ThemeProvider.js";
-
-export const useThemeContext = () => {
-  const theme =  useContext(ThemeContext);
-  if (!theme) throw new Error("useThemeContext must be used within ThemeProvider");
-  return theme;
-}
